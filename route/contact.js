31fbf0a4b168c6c3fd81ef379f49be8ee3dbf2c8
remi <remi.petit93370@gmail.com>
2025-09-17 17:01:45 +0200

    ajout login + register + contacts swagger

--- route/contact.js
+++ route/contact.js
@@ -4,6 +4,31 @@ const router = express.Router();
 const { getAllContacts } = require('../controller/contact');
 
 // Route pour récupérer tous les utilisateurs
+/**
+ * @openapi
+ * /contacts:
+ *   get:
+ *     tags: [Contacts]
+ *     summary: Récupère la liste des contacts (nécessite un token)
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Liste des contacts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   name:
+ *                     type: string
+ *                   email:
+ *                     type: string
+ *       401:
+ *         description: Non autorisé (token manquant ou invalide)
+ */
 router.get('/', getAllContacts);
 
 module.exports = router;
