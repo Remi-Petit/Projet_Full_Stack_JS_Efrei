bbac753f9ef579b2c0865deaba5f654203365b4a
remi <remi.petit93370@gmail.com>
2025-10-07 09:17:01 +0200

    init frontend react

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
-import { fileURLToPath, URL } from 'node:url';
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // Charge les variables d'environnement selon le mode
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    },
-    server: {
-      host: env.VITE_HOST,
-      port: Number(env.VITE_PORT),
-    },
-  }
+export default defineConfig({
+  plugins: [react()],
 })
