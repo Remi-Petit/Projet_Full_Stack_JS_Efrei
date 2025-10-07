8e56be20860c388d430db82a83d939877da86525
remi <remi.petit93370@gmail.com>
2025-10-07 09:25:47 +0200

    run with environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { fileURLToPath, URL } from 'node:url';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Charge les variables d'environnement selon le mode
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      host: env.VITE_HOST,
+      port: Number(env.VITE_PORT),
+    },
+  }
 })
