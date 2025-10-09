f78c89a8db4dc7493ec9cbb866785aff81fc1105
Remi Petit <remi.petit93370@gmail.com>
2025-10-10 00:07:47 +0200

    fix config env error

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,7 +1,7 @@
 // src/config/env.ts
 
 export const ENV = Object.freeze({
-  API_URL: import.meta.env.VITE_PROTOCOL_API + '://' + import.meta.env.VITE_HOST_API + ':' + import.meta.env.VITE_PORT_API + '/api' || import.meta.env.VITE_API_URL,
+  API_URL: import.meta.env.VITE_API_URL || import.meta.env.VITE_PROTOCOL_API + '://' + import.meta.env.VITE_HOST_API + ':' + import.meta.env.VITE_PORT_API + '/api',
   APP_NAME: import.meta.env.VITE_APP_NAME ?? 'MyApp',
   MODE: import.meta.env.VITE_ENV_MODE ?? 'development',
   VITE_PROTOCOL: import.meta.env.VITE_PROTOCOL,
